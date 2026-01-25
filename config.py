ab037ac26f5e28c689056dc44f04af5e809e1ece
giltnix <nika.shalonina@gmail.com>
2026-01-25 19:40:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
-BOT_TOKEN = "8281276886:AAFH0HizBFyieJyoP0crR9r1nVrKyAtyX3g"
+import os
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+if not BOT_TOKEN:
+    raise RuntimeError("BOT_TOKEN not found in environment variables")BOT_TOKEN = "8281276886:AAFH0HizBFyieJyoP0crR9r1nVrKyAtyX3g"
