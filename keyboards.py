c59e2ad69c65407501f84e047b591f16047e078b
giltnix <nika.shalonina@gmail.com>
2026-01-26 15:35:30 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -12,25 +12,12 @@ def start_keyboard() -> ReplyKeyboardMarkup:
     )
 
 
-def exam_tasks_keyboard(tasks: list[str]) -> ReplyKeyboardMarkup:
+def tasks_keyboard(tasks: list[str]) -> ReplyKeyboardMarkup:
     keyboard = [[KeyboardButton(text=task)] for task in tasks]
-    keyboard.append([KeyboardButton(text="⬅️ Назад")])
+    keyboard.append([KeyboardButton(text="Назад")])
+
     return ReplyKeyboardMarkup(
         keyboard=keyboard,
-        resize_keyboard=True
-    )
-
-
-def topics_keyboard() -> ReplyKeyboardMarkup:
-    return ReplyKeyboardMarkup(
-        keyboard=[
-            [KeyboardButton(text="Present"), KeyboardButton(text="Past")],
-            [KeyboardButton(text="Future")],
-            [KeyboardButton(text="Модальные глаголы")],
-            [KeyboardButton(text="Условные предложения")],
-            [KeyboardButton(text="Косвенная речь")],
-            [KeyboardButton(text="Назад")]
-        ],
         resize_keyboard=True
     )
 
