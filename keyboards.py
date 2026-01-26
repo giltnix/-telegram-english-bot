4280502bb1122dca5549e783f19249cf0f539d32
giltnix <nika.shalonina@gmail.com>
2026-01-26 15:31:14 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,15 +1,18 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-def exam_keyboard() -> ReplyKeyboardMarkup:
+
+def start_keyboard() -> ReplyKeyboardMarkup:
     return ReplyKeyboardMarkup(
         keyboard=[
             [KeyboardButton(text="ОГЭ")],
-            [KeyboardButton(text="ЕГЭ")]
+            [KeyboardButton(text="ЕГЭ")],
+            [KeyboardButton(text="Конкретные темы")]
         ],
         resize_keyboard=True
     )
 
-def tasks_keyboard(tasks: list) -> ReplyKeyboardMarkup:
+
+def exam_tasks_keyboard(tasks: list[str]) -> ReplyKeyboardMarkup:
     keyboard = [[KeyboardButton(text=task)] for task in tasks]
     keyboard.append([KeyboardButton(text="⬅️ Назад")])
     return ReplyKeyboardMarkup(
@@ -17,9 +20,29 @@ def tasks_keyboard(tasks: list) -> ReplyKeyboardMarkup:
         resize_keyboard=True
     )
 
-def answers_keyboard() -> ReplyKeyboardMarkup:
+
+def topics_keyboard() -> ReplyKeyboardMarkup:
     return ReplyKeyboardMarkup(
-        keyboard=[[KeyboardButton(text="A"), KeyboardButton(text="B"), KeyboardButton(text="C")]],
+        keyboard=[
+            [KeyboardButton(text="Present"), KeyboardButton(text="Past")],
+            [KeyboardButton(text="Future")],
+            [KeyboardButton(text="Модальные глаголы")],
+            [KeyboardButton(text="Условные предложения")],
+            [KeyboardButton(text="Косвенная речь")],
+            [KeyboardButton(text="Назад")]
+        ],
         resize_keyboard=True
     )
 
+
+def answers_keyboard() -> ReplyKeyboardMarkup:
+    return ReplyKeyboardMarkup(
+        keyboard=[
+            [
+                KeyboardButton(text="A"),
+                KeyboardButton(text="B"),
+                KeyboardButton(text="C")
+            ]
+        ],
+        resize_keyboard=True
+    )
