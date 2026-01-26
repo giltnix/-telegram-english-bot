77e3781d0eb5f558b84a26c2e8dcb4063cb6e199
giltnix <nika.shalonina@gmail.com>
2026-01-26 17:02:52 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -3,31 +3,23 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 def start_keyboard() -> ReplyKeyboardMarkup:
     return ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text="ОГЭ")],
-            [KeyboardButton(text="ЕГЭ")],
-            [KeyboardButton(text="Конкретные темы")]
+            [KeyboardButton("ОГЭ")],
+            [KeyboardButton("ЕГЭ")],
+            [KeyboardButton("Конкретные темы")]
         ],
         resize_keyboard=True
     )
 
 def tasks_keyboard(tasks: list[str]) -> ReplyKeyboardMarkup:
-    """
-    Создаёт клавиатуру с темами для выбранного режима
-    """
-    keyboard = [[KeyboardButton(text=task)] for task in tasks]
-    keyboard.append([KeyboardButton(text="Назад")])
+    keyboard = [[KeyboardButton(task)] for task in tasks]
+    keyboard.append([KeyboardButton("Назад")])
     return ReplyKeyboardMarkup(
         keyboard=keyboard,
         resize_keyboard=True
     )
 
 def answers_keyboard() -> ReplyKeyboardMarkup:
-    """
-    Кнопки для выбора ответа A, B, C
-    """
     return ReplyKeyboardMarkup(
-        keyboard=[
-            [KeyboardButton(text="A"), KeyboardButton(text="B"), KeyboardButton(text="C")]
-        ],
+        keyboard=[[KeyboardButton("A"), KeyboardButton("B"), KeyboardButton("C")]],
         resize_keyboard=True
     )
