0699744002c7171c870d66db256a69435730e57c
giltnix <nika.shalonina@gmail.com>
2026-01-26 16:04:57 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -10,7 +10,6 @@ def start_keyboard() -> ReplyKeyboardMarkup:
         resize_keyboard=True
     )
 
-
 def tasks_keyboard(tasks: list[str]) -> ReplyKeyboardMarkup:
     keyboard = [[KeyboardButton(text=task)] for task in tasks]
     keyboard.append([KeyboardButton(text="Назад")])
@@ -18,7 +17,6 @@ def tasks_keyboard(tasks: list[str]) -> ReplyKeyboardMarkup:
         keyboard=keyboard,
         resize_keyboard=True
     )
-
 
 def answers_keyboard() -> ReplyKeyboardMarkup:
     return ReplyKeyboardMarkup(
